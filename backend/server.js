154fd551c9626bccace0a5c10d88cad44fcdc728
Nila <sangeetha9223@gmail.com>
2020-09-24 10:47:49 +0530

    es6

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+// const express = require('express');
+// const path = require('path');
 
 const app = express();
 
